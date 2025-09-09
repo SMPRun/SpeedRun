e4358c0de2c083fa4b9208e73697a877aa0f959a
Emmerich <emmerichhbrowne@gmail.com>
2025-09-09 16:24:44 -0500

    feat: init

---
+++ src/main/kotlin/net/smprun/speedrun/Speedrun.kt
@@ -0,0 +1,14 @@
+package net.smprun.speedrun
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Speedrun : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
