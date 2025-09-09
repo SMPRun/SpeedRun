e2ef81375f70ef7cb6299af553737d88cac21be6
Emmerich <emmerichhbrowne@gmail.com>
2025-09-09 17:49:47 -0500

    feat: auto register

--- src/main/kotlin/net/smprun/speedrun/Speedrun.kt
+++ src/main/kotlin/net/smprun/speedrun/Speedrun.kt
@@ -1,14 +1,27 @@
 package net.smprun.speedrun
 
+import co.aikar.commands.PaperCommandManager
+import com.tcoded.folialib.FoliaLib
+import net.smprun.speedrun.utils.RegistrationManager
 import org.bukkit.plugin.java.JavaPlugin
 
 class Speedrun : JavaPlugin() {
 
+    lateinit var foliaLib: FoliaLib
+    lateinit var commandManager: PaperCommandManager
+    private lateinit var registrationManager: RegistrationManager
+
     override fun onEnable() {
-        // Plugin startup logic
+        foliaLib = FoliaLib(this)
+        commandManager = PaperCommandManager(this)
+        registrationManager = RegistrationManager(this)
+        
+        registrationManager.registerAll()
+        
+        logger.info("Speedrun plugin enabled!")
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        logger.info("Speedrun plugin disabled!")
     }
 }
---
+++ src/main/kotlin/net/smprun/speedrun/annotations/AutoRegister.kt
@@ -0,0 +1,5 @@
+package net.smprun.speedrun.annotations
+
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class AutoRegister
