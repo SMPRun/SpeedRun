d2b12958bd17dcda939e68e31653d162c04117b0
emmerichdev <emmerichhbrowne@gmail.com>
2025-10-14 16:56:17 -0500

    feat: simpler scoreboard setup

--- src/main/kotlin/net/smprun/speedrun/Speedrun.kt
+++ src/main/kotlin/net/smprun/speedrun/Speedrun.kt
@@ -6,7 +6,7 @@ import net.smprun.common.CommonServices
 import net.smprun.speedrun.game.GameService
 import net.smprun.common.database.MongoService
 import net.smprun.common.utils.RegistrationManager
-import net.smprun.speedrun.scoreboard.ScoreboardService
+import net.smprun.speedrun.scoreboard.SpeedrunScoreboard
 import org.bukkit.plugin.java.JavaPlugin
 
 class Speedrun : JavaPlugin() {
@@ -16,7 +16,7 @@ class Speedrun : JavaPlugin() {
     private lateinit var registrationManager: RegistrationManager
     lateinit var mongoService: MongoService
     lateinit var gameService: GameService
-    lateinit var scoreboardService: ScoreboardService
+    lateinit var scoreboardService: SpeedrunScoreboard
 
     override fun onEnable() {
         foliaLib = FoliaLib(this)
@@ -29,16 +29,14 @@ class Speedrun : JavaPlugin() {
         gameService = GameService(this)
         registrationManager.registerAll()
 
-        scoreboardService = ScoreboardService(this)
-        scoreboardService.start()
+        scoreboardService = SpeedrunScoreboard(this)
+        CommonServices.registerScoreboard(scoreboardService)
         
         logger.info("Speedrun plugin enabled!")
     }
 
     override fun onDisable() {
-        if (this::scoreboardService.isInitialized) {
-            scoreboardService.close()
-        }
+        // Scoreboard closed by Common automatically
         logger.info("Speedrun plugin disabled!")
     }
 }
---
+++ src/main/kotlin/net/smprun/speedrun/scoreboard/SpeedrunScoreboard.kt
@@ -0,0 +1,53 @@
+package net.smprun.speedrun.scoreboard
+
+import kotlinx.coroutines.runBlocking
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import net.megavex.scoreboardlibrary.api.sidebar.Sidebar
+import net.smprun.common.scoreboard.ScoreboardService
+import net.smprun.common.utils.Colors
+import net.smprun.common.utils.TimeUtil
+import net.smprun.speedrun.Speedrun
+import net.smprun.speedrun.player.repository.PlayerStatsRepository
+import org.bukkit.entity.Player
+
+class SpeedrunScoreboard(private val speedrunPlugin: Speedrun) : ScoreboardService(speedrunPlugin, speedrunPlugin.foliaLib) {
+
+    private val playerStatsRepository by lazy { PlayerStatsRepository(speedrunPlugin) }
+
+    override fun getTitle(player: Player): Component {
+        return Component.text("SMPRun Network", Colors.BRAND_PRIMARY)
+    }
+
+    override fun initializeSidebar(player: Player, sidebar: Sidebar) {
+        sidebar.line(0, Component.text("Best Time: ", Colors.BRAND_SECONDARY).append(Component.text("N/A", NamedTextColor.WHITE)))
+        sidebar.line(1, Component.text("Recent Time: ", Colors.BRAND_SECONDARY).append(Component.text("N/A", NamedTextColor.WHITE)))
+        sidebar.line(2, Component.text("Wins: ", Colors.BRAND_SECONDARY).append(Component.text("N/A", NamedTextColor.WHITE)))
+        sidebar.line(3, Component.text("Current Run: ", Colors.BRAND_SECONDARY).append(Component.text("N/A", NamedTextColor.WHITE)))
+        sidebar.line(4, Component.text(""))
+        sidebar.line(5, Component.text("smprun.net", NamedTextColor.GRAY))
+    }
+
+    override fun fetchScoreboardLines(player: Player): List<Component> {
+        val stats = runBlocking { playerStatsRepository.findByUuid(player.uniqueId) }
+
+        val bestTimeText = stats?.bestTime?.let { TimeUtil.formatTimeShort(it) } ?: "N/A"
+        val recentTimeText = stats?.recentTime?.let { TimeUtil.formatTimeShort(it) } ?: "N/A"
+        val winsText = stats?.wins?.toString() ?: "N/A"
+
+        val currentRunText = if (speedrunPlugin.gameService.isGameActive) {
+            speedrunPlugin.gameService.getGameDuration()?.let { TimeUtil.formatTimeShort(it) } ?: "N/A"
+        } else {
+            "N/A"
+        }
+
+        return listOf(
+            Component.text("Best Time: ", Colors.BRAND_SECONDARY).append(Component.text(bestTimeText, NamedTextColor.WHITE)),
+            Component.text("Recent Time: ", Colors.BRAND_SECONDARY).append(Component.text(recentTimeText, NamedTextColor.WHITE)),
+            Component.text("Wins: ", Colors.BRAND_SECONDARY).append(Component.text(winsText, NamedTextColor.WHITE)),
+            Component.text("Current Run: ", Colors.BRAND_SECONDARY).append(Component.text(currentRunText, NamedTextColor.WHITE)),
+            Component.text(""),
+            Component.text("smprun.net", NamedTextColor.GRAY)
+        )
+    }
+}
